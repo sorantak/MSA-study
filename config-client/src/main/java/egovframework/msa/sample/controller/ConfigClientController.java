c8979b8e065e1c8076462a9cf3d30337a47017e1
탁한솔 <takyhsoul@etoos.com>
2022-04-19 17:36:17 +0900

    Fix config-client setting

--- config-client/src/main/java/egovframework/msa/sample/controller/ConfigClientController.java
+++ config-client/src/main/java/egovframework/msa/sample/controller/ConfigClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
-    @Value("${config.profile")
+    @Value("${config.profile}")
     private String profile;
 
     @Value("${config.message}")
