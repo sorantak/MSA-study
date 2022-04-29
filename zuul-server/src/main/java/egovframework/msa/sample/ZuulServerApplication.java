084af71b4ba10d151e2e3f8dfcdf5700bf4d2b70
sorantak <takyhsoul@gmail.com>
2022-04-29 16:59:01 +0900

    Add : Circuit Breaker on gateway

--- zuul-server/src/main/java/egovframework/msa/sample/ZuulServerApplication.java
+++ zuul-server/src/main/java/egovframework/msa/sample/ZuulServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +17,17 @@ public class ZuulServerApplication {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
 
+    /** Default Resilience4j circuit breaker configuration */
+//    @Bean
+//    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+//        return factory ->
+//
+//                factory.configureDefault(
+//                        id ->
+//                                new Resilience4JConfigBuilder(id)
+//                                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+//                                        .timeLimiterConfig(
+//                                                TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+//                                        .build());
+//    }
 }
